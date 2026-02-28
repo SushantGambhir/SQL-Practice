4cfe28180a60319e2dc92c6efcb16d5eeee4596a
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-02-28 12:13:14 +0530

    Create table

---
+++ 1_CreatingInsertingInTables.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Employees(
+	EmployeeId int,
+	EmployeeName varchar(100),
+	DOJ datetime,
+	Salary float
+)
+
+INSERT INTO Employees(EmployeeId,EmployeeName,DOJ,Salary)
+VALUES
+(1,'Sushant','2024-01-12',82000),
+(2,'John','2023-01-14',40000),
+(3,'Aman','2022-11-24',35000)
+
+--DELETE FROM Employees
+--SELECT * FROM Employees
