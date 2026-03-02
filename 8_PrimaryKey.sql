9fe965dcf1c397b4cd7f08baf5245d1c396e1667
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-03-02 22:49:21 +0530

    primary key

--- 8_PrimaryKey.sql
+++ 8_PrimaryKey.sql
@@ -18,3 +18,17 @@ VALUES(1,'Jack','2022-11-24',30000)
 
 --Only one primary key allowed in a table
 --But primary key can have one or more columns
+
+DROP TABLE Employees
+
+-- NOT NULL does not allow NULL values
+CREATE TABLE Employees(
+	EmployeeID INT PRIMARY KEY,
+	EmployeeName VARCHAR(100) NOT NULL,
+	DOJ DATETIME,
+	Salary FLOAT
+)
+
+-- Now this will give error
+INSERT INTO Employees
+VALUES(1,NULL,'2022-11-24',30000)
