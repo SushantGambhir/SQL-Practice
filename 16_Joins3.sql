9b5e3fce7d6b09d456146f6dcab82c6b3b08b8ff
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-03-14 12:07:22 +0530

    join

--- 16_Joins3.sql
+++ 16_Joins3.sql
@@ -11,3 +11,13 @@ ON P.ProductKey = F.ProductKey
 JOIN DimCustomer C
 ON F.CustomerKey = C.CustomerKey
 GROUP BY C.FirstName, P.EnglishProductName
+
+-- Alternatively we can firstly join F and C. Then join P
+SELECT C.FirstName, P.EnglishProductName,
+SUM(F.SalesAmount) AS TotalSales
+FROM DimCustomer C
+JOIN FactInternetSales F
+ON C.CustomerKey = F.CustomerKey
+JOIN DimProduct P
+ON P.ProductKey = F.ProductKey
+GROUP BY C.FirstName, P.EnglishProductName
