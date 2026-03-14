59bbcf9c59d13987bd12ed54ccea07b5f610e437
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-03-14 11:12:39 +0530

    join

--- 15_Joins2.sql
+++ 15_Joins2.sql
@@ -13,3 +13,11 @@ SELECT P.ProductKey, P.EnglishProductName, F.SalesAmount
 FROM DimProduct P
 LEFT JOIN FactInternetSales F
 ON P.ProductKey = F.ProductKey
+
+-- Left Anti Join with DimProduct on Left
+-- A left anti-join returns only the rows from the left table that have no matching rows in the right table
+SELECT P.ProductKey, P.EnglishProductName
+FROM DimProduct P
+LEFT JOIN FactInternetSales F
+ON P.ProductKey = F.ProductKey
+WHERE F.ProductKey IS NULL
