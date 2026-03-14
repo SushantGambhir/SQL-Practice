8b966266ab0f2730c96d8ed467044102459f04c3
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-03-14 12:02:12 +0530

    join

--- 16_Joins3.sql
+++ 16_Joins3.sql
@@ -1,3 +1,8 @@
+-- Now we want to join three tables
+-- DimProduct P, FactInternetSales F, DimCustomer C
+-- F and P have common attribute, F and C have a common attribute. P and C dont have a common attribute
+
+-- We can firstly join F and P. Then join resulting table with C
 SELECT C.FirstName, P.EnglishProductName, 
 SUM(F.SalesAmount) AS TotalSales
 FROM DimProduct P
