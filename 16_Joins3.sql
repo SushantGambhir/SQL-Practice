fa43f84308bf6b5baa17787ece240c3e7756e6c6
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-03-14 11:58:47 +0530

    join

--- 16_Joins3.sql
+++ 16_Joins3.sql
@@ -1,6 +1,8 @@
-SELECT C.FirstName, P.EnglishProductName, F.SalesAmount
+SELECT C.FirstName, P.EnglishProductName, 
+SUM(F.SalesAmount) AS TotalSales
 FROM DimProduct P
 JOIN FactInternetSales F
 ON P.ProductKey = F.ProductKey
 JOIN DimCustomer C
 ON F.CustomerKey = C.CustomerKey
+GROUP BY C.FirstName, P.EnglishProductName
