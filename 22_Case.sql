6e677236b248b993a53cd211cb167efb7fa672bb
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-03-24 21:34:47 +0530

    case

---
+++ 22_Case.sql
@@ -0,0 +1,30 @@
+SELECT ProductKey, EnglishProductName, Color
+FROM DimProduct
+
+SELECT ProductKey, EnglishProductName, Color,
+CASE Color
+    WHEN 'Red' THEN 'R'
+    ELSE 'NA'
+END
+FROM DimProduct
+WHERE Color <> 'NA'
+
+SELECT ProductKey, EnglishProductName, Color,
+CASE Color
+    WHEN 'Red' THEN 'R'
+    WHEN 'Silver' THEN 'S'
+    WHEN 'White' THEN 'W'
+    ELSE 'NA'
+END
+AS ColorCode
+FROM DimProduct
+WHERE Color <> 'NA'
+
+SELECT ProductKey, EnglishProductName, Color,
+CASE
+    WHEN ListPrice>=3000 THEN 'High'
+    WHEN ListPrice>=2000 THEN 'Average'
+    ELSE 'Low'
+END
+AS PriceLevel
+FROM DimProduct
