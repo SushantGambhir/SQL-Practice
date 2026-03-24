b5984069a96d16207f2437869a706851716774a8
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-03-24 21:22:40 +0530

    iif

--- 21_IIFFunction.sql
+++ 21_IIFFunction.sql
@@ -6,3 +6,13 @@ FROM DimProduct
 
 SELECT IIF(10=10,1,0)
 SELECT IIF(10=100,'T','F')
+
+SELECT ProductKey, EnglishProductName, Color,
+IIF(Color='Red','Yes','No') AS IsRed
+FROM DimProduct
+
+-- Nested IIF
+SELECT ProductKey, EnglishProductName, Color,
+IIF(Color='Red','Yes',IIF(Color='Silver','Silver','Neither')) 
+AS IsRedOrSilver
+FROM DimProduct
