5a04edbd16408b61057fa6ba7f226941ed77903c
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-03-24 21:19:14 +0530

    iif

--- 21_IIFFunction.sql
+++ 21_IIFFunction.sql
@@ -2,4 +2,7 @@ SELECT ProductKey, EnglishProductName, Color
 FROM DimProduct
 
 -- IIF Function only available in MS SQL Server
+-- Syntax: IIF(Statement, O/P if True, O/P if False)
 
+SELECT IIF(10=10,1,0)
+SELECT IIF(10=100,'T','F')
