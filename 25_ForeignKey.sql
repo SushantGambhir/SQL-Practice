2ab91fabdb66267ab62d22c8e22c75566129cad0
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-03-25 22:20:56 +0530

    foreign key

--- 25_ForeignKey.sql
+++ 25_ForeignKey.sql
@@ -15,6 +15,14 @@ CREATE TABLE SalesTran2(
     SALES DECIMAL(10,2)
 )
 
+INSERT INTO Products2
+VALUES
+(1,'ABC Logo Cap',300.00),
+(2,'Ball Bearing',500.00),
+(3,'Bell',75.20),
+(4,'Trousers',83.80),
+(5,'Shirt',200.00)
+
 INSERT INTO SalesTran2
 VALUES
 (1,'SOB982',10,3000.00),
@@ -23,11 +31,3 @@ VALUES
 (1,'SOB985',7,2100.00),
 (2,'SOB986',8,4000.00),
 (4,'SOB987',10,838.00)
-
-INSERT INTO Products2
-VALUES
-(1,'ABC Logo Cap',300.00),
-(2,'Ball Bearing',500.00),
-(3,'Bell',75.20),
-(4,'Trousers',83.80),
-(5,'Shirt',200.00)
