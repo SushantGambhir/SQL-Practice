55b57d0e5d2d819985c9ab83a1e56b0f5dee3c12
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-04-07 21:55:14 +0530

    having

--- 30_HAVING.sql
+++ 30_HAVING.sql
@@ -3,3 +3,12 @@ SELECT ProductKey, SUM(SalesAmount) AS TotalSales
 FROM FactInternetSales
 GROUP BY ProductKey
 HAVING SUM(SalesAmount)>=100000
+
+--WHERE clause filters rows before grouping or aggregation, while the HAVING clause filters groups after aggregation
+
+--Query all those Product Keys and Total Sales where Total Sales are above 100000 from the year 2012
+SELECT ProductKey, SUM(SalesAmount) AS TotalSales
+FROM FactInternetSales
+WHERE YEAR(OrderDate) = 2012
+GROUP BY ProductKey
+HAVING SUM(SalesAmount)>=100000
