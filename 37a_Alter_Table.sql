d6ffd4daaa64aa6fd5af810b5e0363d6132d404c
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-04-10 19:44:18 +0530

    Alter Table

--- 37a_Alter_Table.sql
+++ 37a_Alter_Table.sql
@@ -31,3 +31,19 @@ ALTER TABLE EmpNew
 ADD CONSTRAINT Pk_empid PRIMARY KEY(EmpId)
 -- Here in the above query 'Pk_empid' is optional, we are just giving a name to the constraint
 -- The above query will work only if all entries in the column satisfies prerequisites to be the primary key
+
+CREATE TABLE Salaries(
+    EmpId INT,
+    Salary DECIMAL(10,2)
+)
+
+ALTER TABLE Salaries
+ADD CONSTRAINT Fk_empid FOREIGN KEY(EmpId)
+REFERENCES EmpNew(EmpId)
+
+-- Remove constraints which were defined during table creation
+ALTER TABLE Salaries
+DROP Fk_empid
+
+ALTER TABLE EmpNew
+DROP Pk_empid
