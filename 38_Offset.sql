bc87858b32e8cea9bef62a3b6782d9b5f6b37c27
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-04-11 18:29:06 +0530

    Offset

--- 38_Offset.sql
+++ 38_Offset.sql
@@ -6,3 +6,16 @@ SELECT TOP 10 * FROM (
     SELECT TOP 20 * FROM DimProduct ORDER BY ProductKey
 ) AS First20
 ORDER BY ProductKey DESC;
+
+-- Way 2: Using OFFSET
+-- The below query will ignore first 10 rows and display all the reamining ones
+SELECT * FROM DimProduct
+ORDER BY ProductKey
+OFFSET 10 ROWS
+
+-- Row 11 to 20
+SELECT * FROM DimProduct
+ORDER BY ProductKey
+OFFSET 10 ROWS
+FETCH FIRST 10 ROWS ONLY
+-- We could have also used 'FETCH NEXT 10 ROWS ONLY' instead
