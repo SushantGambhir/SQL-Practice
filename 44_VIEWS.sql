7ca2939a7927fddb194904e6c8c29e5ff8bc05e6
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-04-12 21:43:59 +0530

    Views

--- 44_VIEWS.sql
+++ 44_VIEWS.sql
@@ -7,3 +7,15 @@ SELECT * FROM DimProduct
 SELECT * FROM VwSelectData
 
 -- View acts as a security layer on top of the existing table
+
+CREATE VIEW VwJoined
+AS
+SELECT P.EnglishProductName, F.OrderDate, SUM(SalesAmount) AS TotalSales
+FROM DimProduct P JOIN FactInternetSales F
+ON P.ProductKey = F.ProductKey
+GROUP BY P.EnglishProductName,F.OrderDate
+
+SELECT * FROM VwJoined
+
+-- Limitation of VIEW
+-- ORDER BY not possible without TOP
