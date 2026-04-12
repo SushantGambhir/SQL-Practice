8803e64fd695d6dd645519ecea3bdb68fab9beac
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-04-12 15:44:51 +0530

    Rollup cube

---
+++ 42_GROUPBY_ROLLUP_CUBE.sql
@@ -0,0 +1,18 @@
+SELECT YEAR(OrderDate) AS OrderYear,SUM(SalesAmount) AS SalesByYear
+FROM FactInternetSales
+GROUP BY ROLLUP(YEAR(OrderDate))
+-- When we use ROLLUP with GROUP BY there are two differences from the result without it
+-- 1. OrderYear column will be sorted
+-- 2. One extra row will be added at bottom (NULL, Sum Total of all values in SalesByYear)
+
+SELECT YEAR(OrderDate) AS OrderYear, FORMAT(OrderDate,'MMM') AS MonthNm,
+SUM(SalesAmount) AS SalesByYear
+FROM FactInternetSales
+GROUP BY ROLLUP(YEAR(OrderDate),FORMAT(OrderDate,'MMM'))
+-- When we use ROLLUP with GROUP BY there are few differences from the result without it
+-- 1. OrderYear column will be sorted
+-- 2. Month will not be sorted because its considered plain text
+-- 3. There will be one NULL Entry for each Year in Column MonthNum and 
+-- SalesByYear column will have sum total for that year
+-- 4. In the end NULL in OrderYear, MonthNm, and sum total of 
+-- all values in SalesByYear at the bottom of table
