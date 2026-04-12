df299cf39f79e2fc8b84a6b9943651a6d20bd52b
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-04-12 22:03:50 +0530

    CTE

---
+++ 45_CTE1.sql
@@ -0,0 +1,22 @@
+--A Common Table Expression (CTE) is a temporary, named result set defined 
+--using the WITH clause that exists only during the execution of a single query. 
+
+WITH CteSelectData
+AS
+(
+	SELECT ProductKey,YEAR(OrderDate) AS OrderYear,SalesAmount 
+	FROM FactInternetSales
+)
+SELECT * FROM CteSelectData
+-- If we try to run the above SELECT query alone it will not work
+-- We need to select full from 'WITH'
+
+WITH CteSelectData2
+AS
+(
+	SELECT ProductKey,YEAR(OrderDate) AS OrderYear,SalesAmount 
+	FROM FactInternetSales
+)
+SELECT ProductKey,OrderYear,SUM(SalesAmount) AS TotalSales
+FROM CteSelectData2
+GROUP BY ProductKey,OrderYear
