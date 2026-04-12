71f56d37b4ae602fbc9842b8a4596a5385afe418
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-04-12 09:32:13 +0530

    Merge

--- 40_MERGE.sql
+++ 40_MERGE.sql
@@ -15,3 +15,15 @@ WHERE EmployeeId = 6
 UPDATE EmployeesStage
 SET City='NYC'
 WHERE EmployeeId = 5
+
+SET IDENTITY_INSERT Employees ON 
+-- By default we are not allowed to explictly add values for columns with auto-increment
+-- This command is needed to allow it
+
+-- Now we want to ensure that the source (EmployeesStage) table and target (Employees) table are in sync
+MERGE Employees AS T
+USING EmployeesStage AS S
+ON T.EmployeeId=S.EmployeeId
+WHEN NOT MATCHED THEN
+INSERT (EmployeeId,EmpName,City,Department,Salary)
+VALUES(S.EmployeeId,S.EmpName,S.City,S.Department,S.Salary);
