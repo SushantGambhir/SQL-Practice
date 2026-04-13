6c0dbf2c00d7855ec7b0ed7f6ca87daa353cb683
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-04-13 19:20:26 +0530

    variables

--- 47_Variables.sql
+++ 47_Variables.sql
@@ -6,3 +6,15 @@ SET @y = 'HELLO'
 
 SELECT @x -- Fetch the value
 SELECT @y
+
+-- We can DECLARE and SELECT multiple variable in single statement
+-- But cannot set multiple in single statement
+
+DECLARE @x INT, @y VARCHAR(20)
+
+SET @x=10
+SET @y= 'World'
+
+SELECT @x, @y
+
+-- We must run all three (DECLARE, SET, SELECT) together. Only then they will work
