06598d8c199b96b2f5f35b05702125b0bed85e57
Sushant Gambhir <sushantgambhir9878@gmail.com>
2026-04-16 22:06:07 +0530

    if else

--- 48_IFELSE.sql
+++ 48_IFELSE.sql
@@ -1,2 +1,22 @@
 DECLARE @x INT
 SET @x = 10
+
+IF @x = 10
+	BEGIN
+		PRINT('1 is selected')
+	END
+ELSE
+	BEGIN
+		PRINT('0 is selected')
+	END
+
+IF @x = 10
+	BEGIN
+		PRINT('1 is selected')
+		PRINT('Hello')
+	END
+ELSE
+	BEGIN
+		PRINT('0 is selected')
+		PRINT('World')
+	END
